7cbbc783ac6d4eb3ce861d95b1c0b5044c3fab3e
mason <mason.kohls@berkeley.edu>
2019-03-15 23:55:53 -0700

    Formatting

--- VolumeSlider/background.js
+++ VolumeSlider/background.js
@@ -1,9 +1,3 @@
-//audioCtx = new (window.AudioContext)();
-//chrome.tabCapture.capture
-//var source = audioCtx.createMediaStreamSource(stream);
-//var analyser = audioCtx.createAnalyser();
-//createGain
-
 var gain_node;
 var audio_ctx;
 var media_stream;
@@ -12,16 +6,15 @@ var running = false;
 chrome.extension.onConnect.addListener(function(port) {
     port.onMessage.addListener(function(msg) {
         var gain = parseFloat(msg.action);
-        
         if (msg.action === "start") {
-            audio_ctx = new (window.AudioContext)();  
+            audio_ctx = new(window.AudioContext)();
             chrome.tabCapture.capture({
-                    audio : true,
-                    video : false
+                audio: true,
+                video: false
             }, function(stream) {
-                if (chrome.runtime.lastError){
+                if (chrome.runtime.lastError) {
                     console.log(chrome.runtime.lastError.message);
-                } else { 
+                } else {
                     running = true
                     media_stream = stream;
                     var source = audio_ctx.createMediaStreamSource(stream);
@@ -31,24 +24,22 @@ chrome.extension.onConnect.addListener(function(port) {
                     gain_node = audio_ctx.createGain();
                     source.connect(gain_node);
                     gain_node.connect(audio_ctx.destination);
-                    gain_node.gain.value = 1.0; 
+                    gain_node.gain.value = 1.0;
                 }
-        });
+            });
         }
-        
-        if(msg.action == "end") {
+
+        if (msg.action == "end") {
             running = false
             audio_ctx.close();
             media_stream.getAudioTracks()[0].stop();
         }
-        
-        
+
+
         if (isFinite(gain) && running) {
             gain = Math.max(gain, -1);
             gain = Math.min(gain, 4);
             gain_node.gain.setValueAtTime(gain, audio_ctx.currentTime);
-            //gain_node.gain.value = gain;
         }
     });
 });
-
